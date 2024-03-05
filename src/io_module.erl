9651235329f477144665c9134dbab7e59238220f
Yars2021 <79992244+Yars2021@users.noreply.github.com>
2024-03-05 19:17:09 +0300

    Update io_module.erl

--- src/io_module.erl
+++ src/io_module.erl
@@ -41,6 +41,6 @@ parse_point(Line) ->
 
 % Вывод чисел
 print_number(Nums) ->
-    io:format("~s~n", [lists:join("\t", 
+    io:format("~s~n", [lists:join("\t",
                        lists:map(fun(N) -> erlang:float_to_list(float(N), [{decimals, 2}]) end,
                        Nums))]).
